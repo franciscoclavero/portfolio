f2be56b9828ada2a283119587e4e26e574cd45ba
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-01 16:12:26 -0300

    feat: create test for body content

---
+++ src/components/molecules/BodyContent/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from 'react'
+
+import BodyContent from './'
+
+describe('BodyContent component ', () => {
+  it('- render', () => {
+    render(<BodyContent />)
+
+    const bodyContentNode = screen.getByText('AAAA');
+
+    expect(bodyContentNode).toBeInTheDocument();
+  })
+});
