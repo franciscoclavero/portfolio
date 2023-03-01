62daf5924b430678099ce2f27f5afbe1c8add798
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-01 16:12:51 -0300

    feat: add body content

--- src/components/molecules/Background/index.tsx
+++ src/components/molecules/Background/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import BodyContent from '../BodyContent';
 import Ocean from '../Ocean';
 
 import { BackgroundStyled } from './styled.module';
@@ -8,6 +9,7 @@ const Background = () => {
   return (
     <BackgroundStyled>
       <Ocean />
+      <BodyContent />
     </BackgroundStyled>
   )
 }
