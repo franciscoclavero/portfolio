71bdc5a32df34cee5601c6bd2c933c88e322a275
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-01 17:18:50 -0300

    feat: create animation

--- src/components/atoms/MenuItem/styled.module.tsx
+++ src/components/atoms/MenuItem/styled.module.tsx
@@ -6,9 +6,22 @@ export const MenuItemStyled = styled.li`
   line-height: 70px;
   width: 190px;
   cursor: pointer;
+  overflow: hidden;
+  margin-left: 1000px;
+
+  animation-name: surge;
+  animation-duration: 1s;
+  animation-delay: 1s;
+  animation-fill-mode: forwards;    
+
 
   &:hover {
     font-size: 16px;
     border-bottom: 3px solid #fff;
+  }
+
+  @keyframes surge {
+    0%   { margin-left: 1000px; }
+    100% { margin-left: 0; }
   }
 `;
