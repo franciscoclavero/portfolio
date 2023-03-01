73d9a8666572932d65bf446e0a8e743dc6a9034f
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-01 17:02:30 -0300

    feat: create items

---
+++ src/components/atoms/MenuItem/styled.module.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MenuItemStyled = styled.li`
+  height: 70px;
+  text-align: center;
+  line-height: 70px;
+  width: 190px;
+  cursor: pointer;
+
+  &:hover {
+    font-size: 16px;
+    border-bottom: 3px solid #fff;
+  }
+`;
