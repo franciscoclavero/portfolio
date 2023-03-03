56435a4c12e94dce845339939c3e201f9b7796a5
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-03 10:15:32 -0300

    feat: create information data

---
+++ src/DB/informationAreaItems.tsx
@@ -0,0 +1,6 @@
+export const informationAreaItems = {
+  'name': {'title': 'Nome: ', 'value': 'Francisco Guilherme Kluser de Oliveira Clavero'},
+  'year': {'title': 'Idade: ', 'value': '25 anos'},
+  'email': {'title': 'E-mail: ', 'value': 'franciscogkoclavero@gmail.com'},
+  'city': {'title': 'Cidade: ', 'value': 'Praia Grande - SP'},
+};
