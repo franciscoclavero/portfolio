9371104af49901cf5a26cf01a1425a1d4ecf2c49
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-03 09:30:49 -0300

    feat: create component with description

---
+++ src/components/atoms/PersonalDescriptionText/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { TextStyled } from "./style.module";
+
+const PersonalDescriptionText = () => {
+  return (
+    <TextStyled>
+      Me chamo Francisco, sou bacharel em Ciências da Computação com mais de 3 anos de experiência na área. Apaixonado por animes e jogos, muito fã do Nintendo e da franquia Pokémon. Gosto de estar me desafiando e podendo aprender todo dia algo novo.
+    </TextStyled>
+  );
+};
+
+export default PersonalDescriptionText;
---
+++ src/components/atoms/PersonalDescriptionText/style.module.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TextStyled = styled.p`
+  font-size: 16px;
+  width: 500px;
+  text-align: justify;
+  text-indent: 32px;
+`
