4994cf00e6ef40497914a163191f937ae5a978ef
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-06 10:24:22 -0300

    feat: create skills Items

---
+++ src/components/atoms/SkillsBallonItem/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BallonItem } from "./styled.module";
+
+const SkillsBallonItem = () => {
+  return (
+    <BallonItem>
+      <span>Item 1</span>
+    </BallonItem>
+  )
+}
+
+export default SkillsBallonItem;
