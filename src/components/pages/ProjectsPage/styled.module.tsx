d5febef691d0e93127ffd4d2a7e0bb6026f5b354
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-07 11:59:54 -0300

    feat: create page projects

---
+++ src/components/pages/ProjectsPage/styled.module.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BodyProjects = styled.div`
+  width: 100%;
+`;
+
+export const ProjectAreaTitle = styled.h1`
+  color: #fff;
+  text-align: center;
+  font-size: 24px;
+`;
