e34d9980ca8b074940704115d347e63b47bb2ea0
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-07 16:22:54 -0300

    feat: add all Experiences

--- src/db/experienceList.tsx
+++ src/db/experienceList.tsx
@@ -3,16 +3,34 @@ export const experienceList = {
     'title': 'Experiências Profissionais',
     'items': [
       {
-        'period': '2020 - 2021',
-        'mainTitle': 'Ciência da Computação',
-        'subTitle': 'Unoesc São Miguel',
-        'description': 'Cursado Bacharelado por 3 anos.'
+        'period': '2021 - 2022',
+        'mainTitle': 'Programador de Sistemas de Informação',
+        'subTitle': 'Iopoint Tecnologia LTDA',
+        'description': 'Desenvolvimento Full Stack utilizando Angular, Laravel e MySql; Correção de erros e auxilio ao suporte.'
       },
       {
         'period': '2020 - 2021',
-        'mainTitle': 'Ciência da Computação',
-        'subTitle': 'Unoesc São Miguel',
-        'description': 'Cursado Bacharelado por 3 anos.'
+        'mainTitle': 'Tecnico de Apoio em Pesquisa e Desennvolvimento',
+        'subTitle': 'Sicoob São Miguel',
+        'description': 'Análise de metodologia de trabalhado de setores, planejamento e criação de modelos para automatizar o trabalho e assim melhorar o rendimento. Utilização da Plataforma de Automação LECOM junto com Javascript.'
+      },
+      {
+        'period': '2018 - 2020',
+        'mainTitle': 'Suporte Técnico e Desenvolvimento',
+        'subTitle': 'Sicoob São Miguel',
+        'description': 'Manutenção de máquinas, prestação de suporte, desenvolvimento e manutenção de ferramentas utilizando Javascript, PHP, Laravel e MySql. Responsável pelo controle de acesso de toda a Cooperativa.'
+      },
+      {
+        'period': '2017 - 2018',
+        'mainTitle': 'Assistente Administrativo',
+        'subTitle': 'Sicoob São Miguel',
+        'description': 'Atendimento ao cliente, auxilio na utilização dos ATMs.'
+      },
+      {
+        'period': '2016 - 2016',
+        'mainTitle': 'Programador de Sistemas',
+        'subTitle': 'SYSMO Sistemas LTDA',
+        'description': 'Correção, manutenção e desenvolvimento de novas tecnologias utilizando Delphi, Firebird e PostGreSQL.'
       }
     ]
   },
@@ -20,22 +38,28 @@ export const experienceList = {
     'title': 'Cursos',
     'items': [
       {
-        'period': '2020 - 2021',
+        'period': '2015 - 2020',
         'mainTitle': 'Ciência da Computação',
         'subTitle': 'Unoesc São Miguel',
-        'description': 'Cursado Bacharelado por 3 anos.'
+        'description': 'Bacharel em Ciência da Computação'
       },
       {
-        'period': '2020 - 2021',
-        'mainTitle': 'Ciência da Computação',
-        'subTitle': 'Unoesc São Miguel',
-        'description': 'Cursado Bacharelado por 3 anos.'
+        'period': '2012 - 2014',
+        'mainTitle': 'Ensino Médio Completo (Curso Técnico em Informática - EMIEP)',
+        'subTitle': 'Escola de Educação Básica São João Batista',
+        'description': 'Manutenção de máquinas e desenvolvimento de sistemas.'
       },
       {
-        'period': '2020 - 2021',
-        'mainTitle': 'Ciência da Computação',
-        'subTitle': 'Unoesc São Miguel',
-        'description': 'Cursado Bacharelado por 3 anos.'
+        'period': '2014 - 2014',
+        'mainTitle': 'Programador Web',
+        'subTitle': 'SENAI - São Miguel',
+        'description': 'Utilização de HTML, CSS e Javascript.'
+      },
+      {
+        'period': '2013 - 2013',
+        'mainTitle': 'Programador Delphi',
+        'subTitle': 'SENAI - São Miguel',
+        'description': 'Utilização do Delphi.'
       }
     ]
   },
