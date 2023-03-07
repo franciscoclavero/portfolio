9ec38891fd25a31334395d8b4f8670eb303f0788
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-07 16:20:56 -0300

    feat: create footer

---
+++ src/components/atoms/Footer/styled.module.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterStyled = styled.footer`
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: #fff;
+  font-size: 12px;
+  margin-top: 16px;
+  position: absolute;
+  top: 2716px;
+`;
