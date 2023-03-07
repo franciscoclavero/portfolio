64723d4b56f9b3b3b78d9dec68e2f659b39ce7a7
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-03-07 09:46:33 -0300

    refactor: change url imports

--- src/components/templates/BodyContent/index.tsx
+++ src/components/templates/BodyContent/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ExperiencePage from 'src/components/pages/ExperiencePage';
+import ExperiencePage from '../../../components/pages/ExperiencePage';
 
-import HomePage from 'src/components/pages/HomePage';
-import SkillsPage from 'src/components/pages/SkillsPage';
+import HomePage from '../../../components/pages/HomePage';
+import SkillsPage from '../../../components/pages/SkillsPage';
 
 import { BodyContentStyled } from './style.module';
 
